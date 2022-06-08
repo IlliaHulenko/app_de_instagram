04c1b2bbb3efe23eb5972158dfc664ce9beecb9e
Illia Hulenko <finhike86@gmail.com>
2022-06-08 19:35:03 +0200

    confirmar "master"

--- server.js
+++ server.js
@@ -16,14 +16,18 @@ app.get("/users/:userId", getUserById);
 app.post("/users", createUser);
 
 
-
+/**Middoleweare 404 */
 app.use((req, res, next) => {
     res.status(404).send({ status: "error", message: "Not found" });
-})
+});
 
 app.use((error, req, res, next) => {
     console.error(error);
 
     res.statusCode = error.statusCode || 500;
     res.send({ status: "error", message: error.message });
-})
+});
+
+app.listen(SERVER_PORT, () => {
+    console.log(`server listening on http://localhost:${SERVER_PORT}`)
+});
